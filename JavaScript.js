aeb419146deb322582f94b0d51caf67340344a25
print1hello1world <KonradPawlikowski1337@gmail.com>
2016-09-09 12:46:16 +0100

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,1 +1,2 @@
-
+if (window.navigator.userAgent.indexOf("MSIE ") > 0)alert("You are using internet explorer, this may not render properly. please consider upgrading to a modern browser.");
+for (r = 0; r < document.getElementsByTagName('noscript').length; r++) document.getElementsByTagName('noscript') [r].innerHTML = "";
