9465b84419fe6d12e713b60d76732be59e00b696
print1hello1world <KonradPawlikowski1337@gmail.com>
2016-09-24 00:43:48 +1300

    Update Script.js

--- Script.js
+++ Script.js
@@ -2,5 +2,5 @@ if (window.navigator.userAgent.indexOf("MSIE ") > 0)alert("You are using interne
 for (r = 0; r < document.getElementsByTagName('noscript').length; r++) document.getElementsByTagName('noscript') [r].innerHTML = "";
 if(-1==location.href.indexOf("print1hello1world.github.io"))
   {document.body.innerHTML="";document.head.innerHTML="";
-  var Copy_Check=document.createElement("c");
-  blob.innerHTML="Hello dear marker! This individual coppied from<a href='https://print1hello1world.github.io/Computer_Networks/'>https://print1hello1world.github.io/Computer_Networks</a>!!!",document.body.appendChild(Copy_Check) }
+  var Copy_Check=document.createElement("b");
+  blob.innerHTML="Hello dear marker! This individual copied from <a href='https://print1hello1world.github.io/Computer_Networks/'>https://print1hello1world.github.io/Computer_Networks</a>!!!",document.body.appendChild(Copy_Check) }
