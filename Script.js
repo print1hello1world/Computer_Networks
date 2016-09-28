3b35f2a88e3907c3d5ae365d0965deb1c5ce9d69
print1hello1world <KonradPawlikowski1337@gmail.com>
2016-09-28 09:38:24 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,6 +1,6 @@
 if (window.navigator.userAgent.indexOf("MSIE ") > 0)alert("You are using internet explorer, this may not render properly. please consider upgrading to a modern browser.");
 for (r = 0; r < document.getElementsByTagName('noscript').length; r++) document.getElementsByTagName('noscript') [r].innerHTML = "";
-if(-1==location.href.indexOf("print1hello1world.github.io"))
-  {document.body.innerHTML="";document.head.innerHTML="";
-  var Copy_Check=document.createElement("b");
-  blob.innerHTML="Hello dear marker! This individual copied from <a href='https://print1hello1world.github.io/Computer_Networks/'>https://print1hello1world.github.io/Computer_Networks</a>!!!",document.body.appendChild(Copy_Check) }
+//if(-1==location.href.indexOf("print1hello1world.github.io"))
+  //{document.body.innerHTML="";document.head.innerHTML="";
+  //var Copy_Check=document.createElement("b");
+  //blob.innerHTML="Hello dear marker! This individual copied from <a href='https://print1hello1world.github.io/Computer_Networks/'>https://print1hello1world.github.io/Computer_Networks</a>!!!",document.body.appendChild(Copy_Check) }
