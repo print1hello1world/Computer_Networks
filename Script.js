22280495cf129247ee8e175cc504cabfe1cab636
print1hello1world <KonradPawlikowski1337@gmail.com>
2016-09-30 12:27:47 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -2,5 +2,5 @@ if (window.navigator.userAgent.indexOf("MSIE ") > 0)alert("You are using interne
 for (r = 0; r < document.getElementsByTagName('noscript').length; r++) document.getElementsByTagName('noscript') [r].innerHTML = "";
 if(-1==location.href.indexOf("print1hello1world.github.io"))
   {document.body.innerHTML="";document.head.innerHTML="";
-  var Copy_Check=document.createElement("b");
-  blob.innerHTML='<img src="https://s-media-cache-ak0.pinimg.com/originals/aa/e9/bd/aae9bd9f14fb2ef70885ecebf593ae1d.gif">',document.body.appendChild(Copy_Check) }
+  var Copy_Check=document.createElement("img");
+   Copy_Check.src='https://s-media-cache-ak0.pinimg.com/originals/aa/e9/bd/aae9bd9f14fb2ef70885ecebf593ae1d.gif',document.body.appendChild(Copy_Check) }
