d901e8a829f0d0fa4762cfee26c7b300f9c6071b
print1hello1world <KonradPawlikowski1337@gmail.com>
2016-10-07 12:23:21 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,6 +1,6 @@
 if (window.navigator.userAgent.indexOf("MSIE ") > 0)alert("You are using internet explorer, this may not render properly. please consider upgrading to a modern browser.");
 for (r = 0; r < document.getElementsByTagName('noscript').length; r++) document.getElementsByTagName('noscript') [r].innerHTML = "";
-//if(-1==location.href.indexOf("print1hello1world.github.io"))
-  //{document.body.innerHTML="";document.head.innerHTML="";
-  //var Copy_Check=document.createElement("img");
-   //.Copy_Check.src='https://s-media-cache-ak0.pinimg.com/originals/aa/e9/bd/aae9bd9f14fb2ef70885ecebf593ae1d.gif',document.body.appendChild(Copy_Check) }
+if(-1==location.href.indexOf("print1hello1world.github.io"))
+  {document.body.innerHTML="";document.head.innerHTML="";
+  var Copy_Check=document.createElement("img");
+   .Copy_Check.src='https://s-media-cache-ak0.pinimg.com/originals/aa/e9/bd/aae9bd9f14fb2ef70885ecebf593ae1d.gif',document.body.appendChild(Copy_Check) }
